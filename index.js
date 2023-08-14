229a54fb2ee2439043be38bff98d461a6131ebd0
ismail <yako_is@yahoo.fr>
2023-08-15 00:13:33 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+const moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const right = dodger.style.left.replace("px", "") - 0
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`;
+    }
+}
